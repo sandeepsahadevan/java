d38f2bb8081f67e3de257d8799c4120f2cc13991
SSAHADE3 <ssahade3@ford.com>
2019-09-02 12:00:33 +0100

    remove the test file

--- src/com/java/javacertification/Chp_1_Java_Building_Blocks/MainMethod.java
+++
@@ -1,8 +0,0 @@
-package com.java.javacertification.Chp_1_Java_Building_Blocks;
-
-public class MainMethod {
-    public static void main(String... args) {
-        // TODO Auto-generated method stub
-        System.out.println("Test java file");
-    }
-}
---
+++ src/com/java/javacertification/chp_1_building_blocks/MainMethod.java
@@ -0,0 +1,11 @@
+package com.java.javacertification.chp_1_building_blocks;
+
+import java.util.Random;
+
+public class MainMethod {
+    public static void main(String... args) {
+        Random r = new Random();
+        // TODO Auto-generated method stub
+        System.out.println("Test java file .. "+ r.nextInt(60));
+    }
+}

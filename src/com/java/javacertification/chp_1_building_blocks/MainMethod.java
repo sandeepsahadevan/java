864007624dfd313a9aa5e8cfc7a40bc380207747
SSAHADE3 <ssahade3@ford.com>
2019-09-02 14:22:44 +0100

    remove the test file

--- src/com/java/javacertification/chp_1_building_blocks/MainMethod.java
+++ src/com/java/javacertification/chp_1_building_blocks/MainMethod.java
@@ -3,9 +3,20 @@ package com.java.javacertification.chp_1_building_blocks;
 import java.util.Random;
 
 public class MainMethod {
+    private String variable = "first";
+    { System.out.println(" variable first "+ variable); }
+
+    public MainMethod(){
+        variable = "constructor";
+        System.out.println(" variable constructor "+ variable);
+    }
     public static void main(String... args) {
+        MainMethod mm = new MainMethod();
         Random r = new Random();
         // TODO Auto-generated method stub
         System.out.println("Test java file .. "+ r.nextInt(60));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("variable main "+ mm.variable);
     }
+
 }

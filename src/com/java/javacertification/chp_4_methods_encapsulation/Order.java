85f38fa9aaa4030dbd6f8c21a625f34cc999055a
sandeep <sandeep.sahadevan@cognizant.com>
2019-10-07 23:42:03 +0100

    test

---
+++ src/com/java/javacertification/chp_4_methods_encapsulation/Order.java
@@ -0,0 +1,17 @@
+package com.java.javacertification.chp_4_methods_encapsulation;
+
+public class Order {
+    static String result = "";
+
+    {
+        result += "c";
+    }
+
+    static {
+        result += "u";
+    }
+
+    {
+        result += "r";
+    }
+}

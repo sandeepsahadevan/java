c2c57cd23aac3a47d7f1ca5de9184ab606b44982
sandeep <sandeep.sahadevan@cognizant.com>
2019-10-13 15:21:23 +0100

    test

---
+++ src/com/java/javacertification/Capybara.java
@@ -0,0 +1,23 @@
+package com.java.javacertification;
+
+import com.java.javacertification.chp_5_class_design.Rodent;
+
+public class Capybara extends Rodent {
+    String  override = "Child Variable";
+
+    public static void main(String[] args) {
+        Rodent rodent = new Rodent();
+        //Capybara capybara = (Capybara)rodent; // Throws ClassCastException at runtime
+        rodent.override();
+        System.out.println(rodent.override);
+        rodent = new Capybara();
+        rodent.override();
+        System.out.println(rodent.override);
+        Capybara capybara =new Capybara();
+        capybara.override();
+        System.out.println(capybara.override);
+    }
+    public void override(){
+        System.out.println("Child");
+    }
+}
---
+++ src/com/java/javacertification/chp_5_class_design/Rodent.java
@@ -0,0 +1,8 @@
+package com.java.javacertification.chp_5_class_design;
+
+public class Rodent {
+    public String  override = "Parent Variable";
+    public void override(){
+        System.out.println("Parent Method");
+    }
+}
